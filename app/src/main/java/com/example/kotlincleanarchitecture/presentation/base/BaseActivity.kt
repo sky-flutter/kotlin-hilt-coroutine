285acf3de2e051a6947044e8c9fd0908a1a1b3bd
Aakash-PC <aakash@alchemytech.ca>
2021-07-15 00:43:57 +0530

    - Add toast

--- app/src/main/java/com/example/kotlincleanarchitecture/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/example/kotlincleanarchitecture/presentation/base/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kotlincleanarchitecture.presentation.base
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -17,6 +18,10 @@ abstract class BaseActivity<B : ViewDataBinding> : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView<B>(this, layoutId())
         onViewCreated()
+    }
+
+    protected fun showToast(message:String){
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
     }
 
 }
