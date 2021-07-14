0ab489a9f2c56dd9be0c28981433b386383aa386
Aakash-PC <aakash@alchemytech.ca>
2021-07-14 23:36:50 +0530

    - First release

---
+++ app/src/main/java/com/example/kotlincleanarchitecture/presentation/base/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.kotlincleanarchitecture.presentation.base
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel : ViewModel()
---
+++ app/src/main/java/com/example/kotlincleanarchitecture/presentation/fragment/splash/SplashViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.kotlincleanarchitecture.presentation.fragment.splash
+
+import com.example.kotlincleanarchitecture.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor() : BaseViewModel() {
+}
